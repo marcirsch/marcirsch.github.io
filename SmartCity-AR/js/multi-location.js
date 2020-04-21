384c6613662e8288527e5d42dd8feb7a571dc387
Marcell Rausch <marcell.rausch@gmail.com>
2020-04-21 17:38:41 +0200

    task 11,12

---
+++ SmartCity-AR/js/multi-location.js
@@ -0,0 +1,58 @@
+
+// based on  https://medium.com/swlh/build-your-location-based-augmented-reality-web-app-a841956eed2c
+
+window.onload = () => {
+    let places = staticLoadPlaces();
+    renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+   return [
+       {
+           name: 'Magnemite, Lv. 5, HP 10/10',
+           location: {
+               lat: 46.90127795037299,
+               lng: 18.016618859950604,
+           },
+           gtlf: './assets/magnemite/scene.gltf'
+       },
+       {
+           name: 'Articuno, Lv. 80, HP 100/100',
+           location: {
+               lat: 46.90128253197129,
+               lng: 18.01665439922005,
+           },
+           gtlf: './assets/articuno/scene.gltf'
+       },
+       {
+           name: 'Dragonite, Lv. 99, HP 150/150',
+           location: {
+               lat: 46.90130635627607,
+               lng: 18.01661617774159,
+           },
+           gtlf: './assets/dragonite/scene.gltf'
+       },
+   ];
+}
+
+function renderPlaces(places) {
+   let scene = document.querySelector('a-scene');
+
+   places.forEach((place) => {
+       let latitude = place.location.lat;
+       let longitude = place.location.lng;
+
+       let model = document.createElement('a-entity');
+       model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+       model.setAttribute('gltf-model', place.gltf);
+       model.setAttribute('rotation', '0 180 0');
+       model.setAttribute('animation-mixer', '');
+       model.setAttribute('scale', '0.5 0.5 0.5');
+
+       model.addEventListener('loaded', () => {
+           window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+       });
+
+       scene.appendChild(model);
+   });
+}
